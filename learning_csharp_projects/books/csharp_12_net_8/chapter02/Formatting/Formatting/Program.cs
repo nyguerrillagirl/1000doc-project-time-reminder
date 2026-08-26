c7372495f21d5c33e7000ca600c6cfb920b8a95a
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2026-08-26 07:28:46 -0400

    "Day #697 - Working on learning C#"

---
+++ learning_csharp_projects/books/csharp_12_net_8/chapter02/Formatting/Formatting/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
+Console.WriteLine(format: "{0} apples cost {1:C}",
+                  arg0: numberOfApples,
+                  arg1: pricePerApple * numberOfApples);
+string formatted = string.Format(format: "{0} apples cost {1:C}",
+                                 arg0: numberOfApples,
+                                 arg1: pricePerApple * numberOfApples);
+
+
+// Three parameter values can use named arguments.
+Console.WriteLine("{0} {1} lived in {2}.",
+arg0: "Roger", arg1: "Cevung", arg2: "Stockholm");
+
+
+// The following statement must be all on one line when using C# 10
+// or earlier. If using C# 11 or later, we can include a line break
+// in the middle of an expression but not in the string text.
+Console.WriteLine($"{numberOfApples} apples cost {pricePerApple * numberOfApples:C}");
+
+
+string applesText = "Apples";
+int applesCount = 1234;
+string bananasText = "Bananas";
+int bananasCount = 56789;
+
+Console.WriteLine();
+Console.WriteLine(format: "{0,-10} {1,6}",
+arg0: "Name", arg1: "Count");
+
+Console.WriteLine(format: "{0,-10} {1,6:N0}",
+arg0: applesText, arg1: applesCount);
+Console.WriteLine(format: "{0,-10} {1,6:N0}",
+arg0: bananasText, arg1: bananasCount);
---
+++ learning_csharp_projects/books/csharp_12_net_8/chapter02/WorkingWithVariables/WorkingWithVariables/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml;
+
+var population = 67_000_000; // 67 million in UK.
+var weight = 1.88; // in kilograms.
+var price = 4.99M; // in pounds sterling.
+var fruit = "Apples"; // string values use double-quotes.
+var letter = 'Z'; // char values use single-quotes.
+var happy = true; // Booleans can only be true or false.
+
+var xml1 = new XmlDocument();
+XmlDocument xml2 = new XmlDocument();
+
+XmlDocument xml3 = new();   // Target-typed new expression (C# 9.0 and later).
+
+Person kim = new();
+kim.BirthDate = new (1990, 5, 23);
+
+
+Console.WriteLine($"default(int) = {default(int)}");
+Console.WriteLine($"default(bool) = {default(bool)}");
+Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+Console.WriteLine($"default(string) = {default(string)}");
+
+int number = 13;
+Console.WriteLine($"number set to: {number}");
+number = default;
+Console.WriteLine($"number reset to its default: {number}");
+
+class Person
+{
+    public DateTime BirthDate;
+}
