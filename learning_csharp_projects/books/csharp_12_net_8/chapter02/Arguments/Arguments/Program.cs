2979d14271744e28a2c1206a96806b350c2c4fdd
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2026-08-27 07:21:52 -0400

    "Day #698 - Working on C# examples from book reading"

---
+++ learning_csharp_projects/books/csharp_12_net_8/chapter02/Arguments/Arguments/Program.cs
@@ -0,0 +1,8 @@
+﻿WriteLine($"There are {args.Length} arguments."); 
+
+foreach (string arg in args)
+{
+    WriteLine(arg);
+}
+
+
---
+++ learning_csharp_projects/books/csharp_12_net_8/chapter02/ReadingInValues/ReadingInValues/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Write("Type your first name and press Enter: ");
+string firstName = Console.ReadLine();
+
+Write("Type your age and press Enter:");
+string age = Console.ReadLine();
+
+WriteLine($"Hello {firstName}, you are {age} years old.");
