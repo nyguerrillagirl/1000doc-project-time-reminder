6bf7c71063d1abca03be2587bc4801feb0980349
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2026-09-10 07:47:56 -0400

    "Day #712 - Working on Learning C#. Working on chapter 2 questions"

--- learning_csharp_projects/books/csharp_12_net_8/chapter02/Arguments/Arguments/Program.cs
+++ learning_csharp_projects/books/csharp_12_net_8/chapter02/Arguments/Arguments/Program.cs
@@ -1,8 +1,12 @@
-﻿WriteLine($"There are {args.Length} arguments."); 
-
-foreach (string arg in args)
+﻿
+if (args.Length < 3)
 {
-    WriteLine(arg);
+    WriteLine("You must specify two colors and cursor size, e.g.");
+    WriteLine("dotnet run red yellow 50");
+    return; // Stop running.
 }
 
+ForegroundColor = (ConsoleColor)Enum.Parse(enumType: typeof(ConsoleColor), value: args[0],ignoreCase: true);
+BackgroundColor = (ConsoleColor)Enum.Parse(enumType: typeof(ConsoleColor), value: args[1], ignoreCase: true);
+CursorSize = int.Parse(args[2]);
 
---
+++ learning_csharp_projects/books/csharp_12_net_8/chapter02/Arguments/AsyncConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿
+HttpClient client = new HttpClient();
+
+HttpResponseMessage response = 
+    await client.GetAsync("http://www.apple.com");
+
+Console.WriteLine("Apple's home page has {0:N0} bytes.",
+    response.Content.Headers.ContentLength);
+
+
