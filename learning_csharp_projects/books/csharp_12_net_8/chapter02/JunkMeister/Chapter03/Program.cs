f116412db76b613044246ac00351bc8d60741f1e
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2026-09-22 07:23:46 -0400

    "Day #724 - Working on TimeReminder Project"

---
+++ learning_csharp_projects/books/csharp_12_net_8/chapter02/JunkMeister/Chapter03/Program.cs
@@ -0,0 +1,15 @@
+﻿int x = 5;
+int postfixIncrement = x++;
+Console.WriteLine("after postfix increment x: " + x);
+int prefixIncrement = ++x;
+Console.WriteLine("after prefix increment x: " + x);
+
+Type theTypeOfInteger = typeof(int);
+Console.WriteLine("The type of integer is: " + theTypeOfInteger);
+
+string nameOfVariable = nameof(x);
+Console.WriteLine("The name of the variable is: " + nameOfVariable);
+
+int howManyBytesInAnInteger = sizeof(int);
+Console.WriteLine("The number of bytes in an integer is: " + howManyBytesInAnInteger);
+
---
+++ learning_csharp_projects/books/csharp_12_net_8/chapter03/Operators/Chapter03/Program.cs
@@ -0,0 +1,8 @@
+﻿# region Exploring unary operators
+
+int c = 3;
+int d = ++c;
+
+Console.WriteLine($"c: {c}, d: {d}"); // a: 4, d: 4
+
+# endregion 
